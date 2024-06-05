95385594e0fb8b45e514ae0c95204107dd4a9fd3
DevanshuKumawat <devanshukumawat063@gmail.com>
2024-06-05 08:19:07 +0530

    first commit

---
+++ npm/files/test/index.js
@@ -0,0 +1,31 @@
+// const prompt = require ("prompt-sync")()
+
+
+// const msg = prompt('How many more times? ');
+
+// console.log(msg)
+
+//common js
+//module js
+
+// import Password from "password"
+
+// console.log(Password(3))
+ 
+// const Password = require ("password")
+
+// console.log(Password(2))
+
+
+// npm init -y == package.json
+// npm i (package) === node_modules / package-lock.json
+
+
+const cowsay = require ("cowsay")
+
+const hey = cowsay.say({
+    text:"Hello Everyone"
+})
+
+console.log(hey)
+
---
+++ npm/project/index.js
@@ -0,0 +1,43 @@
+// const prompt = require ("prompt-sync")()
+
+
+// const msg = prompt('How many more times? ');
+
+// console.log(msg)
+
+//common js
+//module js
+
+// import Password from "password"
+
+// console.log(Password(3))
+ 
+// const Password = require ("password")
+
+// console.log(Password(2))
+
+
+// npm init -y == package.json
+// npm i (package) === node_modules / package-lock.json
+
+
+// const cowsay = require ("cowsay")
+
+// const hey = cowsay.say({
+//     text:"Hello Everyone"
+// })
+
+// console.log(hey)
+
+// const clock = require ("clock")
+
+// function announceTime(clock) {
+//     clock = clock || Date
+//     return 'It is now ' + clock.now()
+//   }
+   
+//   announceTime()
+//   // It is now 1499904588386
+   
+//   announceTime({now: function () { return 1234 }})
+
